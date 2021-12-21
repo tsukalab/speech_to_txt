b290a91e3a718d99e3f877941400a56e2f392d60
tttottt <fun1759ac@gmail.com>
2021-12-21 12:56:34 +0900

    逐一テキストボックスにに表示させる

--- or_piSimpleGUI_matplotlib.py
+++ or_piSimpleGUI_matplotlib.py
@@ -11,7 +11,6 @@ import PySimpleGUI as sg
 import re
 import sys
 
-
 import speech_to_text_sample_toSaveAudio as stt
 
 # Audio recording parameters
@@ -103,62 +102,92 @@ class Draw_window(object):
 
     def _window(self):
         print("window")
-        ttss = Tts_Result("MIC",2)  
+        # ttsMIC = Tts_Result("MIC",1)
+        ttsMIC = stt.Listen_print(2)
+        # ttsMIKISER = Tts_Result("MIKISER",1)  
+        ttsMIKISER = stt.Listen_print(1)
         while True:
             event, values = self.window.read(timeout=10)
             if event == sg.WIN_CLOSED:
                 break
             elif event == 'Start':
-                print("Start")
-                ttss.get_tts_result()
-                threading.Thread(target=ttss.get_tts_result, daemon=True).start()
+                # 両マイクで認識を開始させる
+                print("Start1")
+                # threading.Thread(target=ttsMIC.main, daemon=True).start()
+                # threading.Thread(target=ttsMIC.get_tts_result, daemon=True).start()
+                print("Start2")
+                threading.Thread(target=ttsMIKISER.main, daemon=True).start()
+                # threading.Thread(target=ttsMIKISER.get_tts_result, daemon=True).start()
+        
             elif event == 'Alarm':
                 message = values[event]
                 sg.popup_auto_close(message)
                 # self.on_closing()
             else:
-                if(ttss.get_FLAG()):
-                    self.window['-M_BOX_2-'].print(ttss.get_result(), text_color='green')
-                    ttss.set_FLAG()
-                # print(event)
-                continue
+            # それぞれの認識時の状態（待機か認識結果出力後か）を判断し，認識結果出力後であればテキストエディタに情報を追加する
+                if(ttsMIC.get_condition()):
+                    print("-M_BOX_2-'MIC")
+                    self.window['-M_BOX_2-'].print("MIC:"+ttsMIC.get_result(), text_color='green')
+                    # self.window['-M_BOX_2-'].print("MIC:"+ttsMIC.get_result(), text_color='green')
+                    ttsMIC.set_condition() #待機状態に戻す
+                    event == 'Start'
+                if(ttsMIKISER.get_condition()):
+                    print("-M_BOX_2-MIK")
+                    self.window['-M_BOX_2-'].print("MIK:"+ttsMIKISER.get_result(), text_color='green')
+                    ttsMIKISER.set_condition() #待機状態に戻す
+                    event == 'Start'
+                    print(event)
+                # print(ttsMIKISER.get_progress_result())
+                self.window['-M_BOX_1-'].update(ttsMIC.get_progress_result())
+                self.window['-M_BOX_1-'].update(ttsMIKISER.get_progress_result())
+                # self.window['-M_BOX_2-'].Widget.clipboard_clear()
+                # self.window['-M_BOX_2-'].print(ttsMIK.get_progress_result(), text_color='green')          
+                # continue
             if event in ('Exit', None):
                 exit(69)
         
         self.window.close()# ウィンドウの破棄と終了
         
 class Tts_Result(object):
-    def __init__(self,devicename, deviceindex):
+    def __init__(self, devicename, deviceindex):        
         self._DEVICE_INDEX =  deviceindex
-        self._RETURN_VALUE = "aiu"
-        self._date = "" 
-        self.tts_FLAG = False
-
+        self.sttPackage = stt.Listen_print(self._DEVICE_INDEX)
+        self._RETURN_VALUE = self.sttPackage.get_result()
+        self._POGRESS_RESULT = self.sttPackage.get_progress_result()
+        self._date = self.sttPackage.get_date() 
+        self.condition = False
+        
     def get_tts_result(self):
         while True:
-            print("Start recognize")
-            text = stt.Listen_print(self._DEVICE_INDEX)
-            text.main()
-            tt = str(text.get_return_value())
-            time = str(text.get_date())
+            # print("Start recognize")
+            # text = stt.Listen_print(self._DEVICE_INDEX)
+            self.sttPackage.main()
+            self.sttPackage.get_progress_result()
+            tt = str(self.sttPackage.get_result())
+            time = str(self.sttPackage.get_date())
             self._RETURN_VALUE= time+tt
-            self.tts_FLAG = True
+            self.condition = True
             # if(devicename=="MIC"):
                 # MIC_RESULT = time+tt 
                 # MIC_FLAG = True
             # return MIC_RESULT
             # win.close_window()
             # win.add_result(MIC_RESULT)
-            return self._RETURN_VALUE
+            # return self._RETURN_VALUE
+            print("End recognize")
             break
+            
     def get_result(self):
         return self._RETURN_VALUE
-
-    def get_FLAG(self):
-        return self.tts_FLAG
     
-    def set_FLAG(self):
-        self.tts_FLAG = False
+    def get_progress_result(self):
+        return self.sttPackage.get_progress_result()
+
+    def get_condition(self):
+        return self.condition
+    
+    def set_condition(self):
+        self.condition = False
 
 if __name__ == '__main__':
     win = Draw_window()
--- speech_to_text_sample_toSaveAudio.py
+++ speech_to_text_sample_toSaveAudio.py
@@ -3,6 +3,7 @@ import datetime
 
 import re
 import sys
+from PySimpleGUI.PySimpleGUI import Tree
 
 from google.cloud import speech_v1 as speech
 
@@ -28,7 +29,7 @@ CHUNK = int(RATE / 10)  # 100ms
 FORMAT = pyaudio.paInt16
 WAVE_OUTPUT_FILENAME = "output.wav"
 
-class MicrophoneStream(object):
+class _MicrophoneStream(object):
     """Opens a recording stream as a generator yielding the audio chunks."""
 
     def __init__(self, rate, chunk, device_index):
@@ -124,12 +125,14 @@ class MicrophoneStream(object):
             print("index"+str(x)+":"+audio.get_device_info_by_index(x).get("name"))
 
 class Listen_print(object):
-    def __init__(self,_deviceindex):
-        self._DEVICE_INDEX =  _deviceindex
-        self._RETURN_VALUE = "aiu"
-        self._date = "" 
+    def __init__(self, deviceindex):
+        self._deviceindex =  deviceindex
+        self._RETURN_VALUE = "RETURN_VALUE"
+        self._date = "00/00/00"
+        self._POGRESS_RESULT = "POGRESS_RESULT"
+        self.condition = False
 
-    def listen_print_loop(self,responses):
+    def _listen_print_loop(self,responses):
         """Iterates through server responses and prints them.
 
         The responses passed is a generator that will block until a response
@@ -146,8 +149,9 @@ class Listen_print(object):
         """
         num_chars_printed = 0
         now = datetime.datetime.now()
+        print(now.strftime('%Y.%m.%d-%H.%M.%S'))
         for response in responses:
-            print(now.strftime('%Y.%m.%d-%H.%M.%S'))
+            
             # mikx  = mikiserCount/(micCount+mikiserCount)
             # micx  = micCount/(micCount+mikiserCount)
             # print(1000*mikx,micCount/1000*micx)    
@@ -163,6 +167,7 @@ class Listen_print(object):
 
             # Display the transcription of the top alternative.
             transcript = result.alternatives[0].transcript
+            print(transcript)
 
             # Display interim results, but with a carriage return at the end of the
             # line, so subsequent lines will overwrite them.
@@ -173,18 +178,20 @@ class Listen_print(object):
 
             if not result.is_final:
                 # print("transcript" + transcript)
-                sys.stdout.write(transcript + overwrite_chars + "\r")
-                sys.stdout.flush()
+                # sys.stdout.write(transcript + overwrite_chars + "\r")
+                # sys.stdout.flush()
 
                 num_chars_printed = len(transcript)
 
+                self._POGRESS_RESULT = transcript + overwrite_chars 
             else:
                 print("transcript + overwrite_chars"+ transcript + overwrite_chars)
                 self._RETURN_VALUE = transcript + overwrite_chars
-                return transcript + overwrite_chars
+                # return transcript + overwrite_chars
 
                 # Exit recognition if any of the transcribed phrases could be
                 # one of our keywords.
+                break
                 if re.search(r"\b(exit|quit)\b", transcript, re.I):
                     print("Exiting..")
                     break
@@ -210,35 +217,74 @@ class Listen_print(object):
             config=config, interim_results=True
         )
 
-        with MicrophoneStream(RATE, CHUNK, self._DEVICE_INDEX) as stream:
-            now = datetime.datetime.now()
-            AUDIO_FILE_NAME = now.strftime('%Y-%m-%d-%H.%M.%S')+".wav"
-            self._date = now.strftime('%Y-%m-%d-%H.%M.%S')
-            AUDIO_FILE_PATH = Bfolder+AUDIO_FILE_NAME
-            # stream.print_deviceList()
+        with _MicrophoneStream(RATE, CHUNK, self._deviceindex) as stream:
             audio_generator = stream.generator()
             requests = (
                 speech.StreamingRecognizeRequest(audio_content=content)
                 for content in audio_generator
             )
+            # 時刻取得
+            now = datetime.datetime.now()
+            AUDIO_FILE_NAME = now.strftime('%Y-%m-%d-%H.%M.%S')+".wav"
+            self._date = now.strftime('%Y-%m-%d-%H.%M.%S')
+            AUDIO_FILE_PATH = Bfolder+AUDIO_FILE_NAME
+            # 使用可能なデバイスの表示
+            # stream.print_deviceList()
             print("【何か話してください】")
             responses = client.streaming_recognize(streaming_config, requests)
+            for response in responses:
+                # mikx  = mikiserCount/(micCount+mikiserCount)
+                # micx  = micCount/(micCount+mikiserCount)
+                # print(1000*mikx,micCount/1000*micx)    
+                if not response.results:
+                    continue
+                # The `results` list is consecutive. For streaming, we only care about
+                # the first result being considered, since once it's `is_final`, it
+                # moves on to considering the next utterance.
+                result = response.results[0]
+                if not result.alternatives:
+                    continue
 
-            # Now, put the transcription responses to use.
-            flag = self.listen_print_loop(responses)
-            print(flag)
-            return flag
-            if(flag):
-                return flag
-                print("end Recogniaze")
-     
-    def get_return_value(self):
+                # Display the transcription of the top alternative.
+                transcript = result.alternatives[0].transcript
+                # overwrite_chars = " " * (num_chars_printed - len(transcript))
+                # print("transcript"+transcript )
+                # print("overwrite_chars"+ overwrite_chars)
+
+                if not result.is_final:
+                    # with open(AUDIO_FILE_PATH, "wb") as f:
+                    #   f.write(audio_generator)
+                    num_chars_printed = len(transcript)
+                    txtlist = transcript
+                    # txtlist = textwrap.wrap(transcript, int(ww/w))
+                    print(txtlist)
+                    lis = [txtlist,0]
+                    # addwriteCsvTwoContents(AUDIO_FILE_NAME = "null", RList = lis, LList = lis, openFileName = "mik.csv", cut_time = 0 , progressTime = mikx)
+                    # micCount = micCount+1
+                    # var.set(txtlist) 
+
+                    lis = [txtlist,0]
+                    # addwriteCsvTwoContents(AUDIO_FILE_NAME = "null", RList = lis, LList = lis, openFileName = "mic.csv", cut_time = 0 , progressTime =  micx)
+                    # mikiserCount = mikiserCount+1 
+                    self._POGRESS_RESULT = txtlist
+                else:
+                    self._RETURN_VALUE = txtlist
+                    break
+            # self._listen_print_loop(responses)
+        
+        self.condition = True
+
+    def get_result(self):
         return self._RETURN_VALUE
-
+    def get_progress_result(self):
+        return self._POGRESS_RESULT
     def get_date(self):
         return self._date
-
-if __name__ == "__main__":
-    a = Listen_print(1)
-    a.main()
+    def get_condition(self):
+        return self.condition 
+    def set_condition(self):
+        self.condition = False
+# if __name__ == "__main__":
+#     a = Listen_print(1)
+#     a.main()
     
