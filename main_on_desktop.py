4d1ef1f11b79c8f7cf9bde058569525e7cfeaf7c
tttottt <fun1759ac@gmail.com>
2022-02-03 13:54:38 +0900

    add: スライダー
    スライダーの追加と認識テキストが出る位置を調整のちほど修正する

--- main_on_desktop.py
+++ main_on_desktop.py
@@ -20,13 +20,17 @@ ttsMIC = stt.Listen_print(MIC_INDEX,"USER", 1)
 ttsMIXER = stt.Listen_print(MIXER_INDEX,"PC",0)
 class display_result():
 # def draw_window():
+
+
+
     def __init__(self):
-        fontsize = 15;fontcolour = "black"
-        num_comment = 3;alpha = 50;bold = "bold"
+        self.fontsize = 15;self.fontcolour = "black"
+        self.num_comment = 3;self.alpha = 50;bold = "bold"
+
 
         self.root = tkinter.Tk()
         ww = self.root.winfo_screenwidth()
-        wh = self.root.winfo_screenheight()  
+        self.wh = self.root.winfo_screenheight()  
         self.root.wm_attributes("-topmost", True)            # ウインドウを最前面へ
         self.root.wm_attributes("-transparentcolor", "white")# ウインドウを設定
         self.root.attributes("-alpha",0.5)                   # 全オブジェクトの透過度を設定:1-0
@@ -34,45 +38,48 @@ class display_result():
         # self.root.protocol("WM_DELETE_WINDOW",  lambda: on_closing(self.root))# windowを閉じれないようにする
         # backgroundカラーに設定されたオブジェクトを完全透過する
         ttk.Style().configure("TP.TFrame", background="white")
-        f = ttk.Frame(master=self.root, style="TP.TFrame", width=ww, height=wh)
-        f.pack()        
+        f = ttk.Frame(master=self.root, style="TP.TFrame", width=ww, height=self.wh)
+        f.pack()   
+        
+             
         self.mic_progres = tkinter.StringVar()
         self.mixer_progres = tkinter.StringVar()
         tmp = ttsMIC.get_progress_result()
         self.mic_progres.set(tmp)
         tmp = ttsMIXER.get_progress_result()
         self.mixer_progres.set(tmp)
-        mic_label = ttk.Label(  self.root, 
+        self.mic_label = ttk.Label(  self.root, 
                                 textvariable=self.mic_progres,
                                 wraplength=ww, 
-                                font=("メイリオ", fontsize, bold),
-                                foreground=fontcolour,
+                                font=("メイリオ", self.fontsize, bold),
+                                foreground=self.fontcolour,
                                 background="red" ) 
-        mixer_label = ttk.Label(    self.root, 
+        self.mixer_label = ttk.Label(    self.root, 
                                     textvariable=self.mixer_progres,
                                     wraplength=ww, 
-                                    font=("メイリオ", fontsize, bold),
-                                    foreground=fontcolour,
+                                    font=("メイリオ", self.fontsize, bold),
+                                    foreground=self.fontcolour,
                                     background="blue" ) 
-        w = mic_label.winfo_reqwidth()/len(tmp)
-        h = mic_label.winfo_reqheight()
-        mixer_label.place(x=0, y=100) 
-        mic_label.place(x=0, y=(wh-num_comment*h-alpha))  # -αは下のタスクバーの分
+        
+        self.mixer_y_place =  self.wh-self.num_comment*self.wh-self.alpha + 100
+        self.mic_y_place   =  self.wh-self.num_comment*self.wh-self.alpha
+        w = self.mic_label.winfo_reqwidth()/len(tmp)
+        h = self.mic_label.winfo_reqheight()
+        self.mixer_label.place(x=0, y=100) 
+        self.mic_label.place(x=0, y=(self.wh-self.num_comment*h-self.alpha))  # -αは下のタスクバーの分
         # # labelmic.place(x=0, y=100) 
         # # labelmikiser.place(x=0+labelmic.winfo_reqwidth(), y=100)  # -αは下のタスクバーの分
     #option_windowの設定        
         option_window = tkinter.Tk()
         option_window.wm_attributes("-topmost", True)
-        option_window.geometry("300x300+"+str(int(ww/2-300/2))+"+"+str(int(wh/2-300/2)))
+        option_window.geometry("300x300+"+str(int(ww/2-300/2))+"+"+str(int(self.wh/2-300/2)))
         option_window.title("Settings")
-        # # Scale（デフォルトで作成）
-        # scaleV = tkinter.Scale(option_window)
-        # scaleV.pack(side = tkinter.RIGHT)
-        # # Scale（オプションをいくつか設定）
-        # self.scale_var = tkinter.DoubleVar()
+    
+        # Scale（オプションをいくつか設定）
+        self.mixer_scale_potision = tkinter.DoubleVar()
         # scaleH = tkinter.Scale(
         #             option_window, 
-        #             variable = self.scale_var, 
+        #             variable = self.mic_progres, 
         #             command = self.slider_scroll,
         #             orient=tkinter.HORIZONTAL,   # 配置の向き、水平(HORIZONTAL)、垂直(VERTICAL)
         #             length = 300,           # 全体の長さ
@@ -87,81 +94,106 @@ class display_result():
         # lnumcomment = ttk.Label(option_window, text="Number of comments",wraplength=ww)
         # lnumcomment.pack()
 
-        # # Scale（オプションをいくつか設定）
-        # self.scale_var = tkinter.DoubleVar()
-        # fontsiza_sv = tkinter.Scale(
-        #             option_window, 
-        #             variable = self.scale_var, 
-        #             command = self.slider_scroll,
-        #             orient=tkinter.HORIZONTAL,   # 配置の向き、水平(HORIZONTAL)、垂直(VERTICAL)
-        #             length = 300,           # 全体の長さ
-        #             width = 20,             # 全体の太さ
-        #             sliderlength = 20,      # スライダー（つまみ）の幅
-        #             from_ = 0,            # 最小値（開始の値）
-        #             to = 20,               # 最大値（終了の値）
-        #             resolution=0.5,         # 変化の分解能(初期値:1)
-        #             tickinterval=5        # 目盛りの分解能(初期値0で表示なし)
-        #             )
-        # fontsiza_sv.pack()
-        # lnumcomment = ttk.Label(option_window, text=str(self.scale_var),wraplength=ww)
-        # lnumcomment.pack()
+        # Scale（オプションをいくつか設定）
+        self.scale_var = tkinter.IntVar()
+        fontsiza_sv = tkinter.Scale(
+                    option_window, 
+                    variable = self.scale_var, 
+                    command = self.slider_scroll,
+                    orient=tkinter.HORIZONTAL,   # 配置の向き、水平(HORIZONTAL)、垂直(VERTICAL)
+                    length = 300,           # 全体の長さ
+                    width = 15,             # 全体の太さ
+                    sliderlength = 20,      # スライダー（つまみ）の幅
+                    from_ = 0,            # 最小値（開始の値）
+                    to = 200,               # 最大値（終了の値）
+                    resolution=1,         # 変化の分解能(初期値:1)
+                    tickinterval=20        # 目盛りの分解能(初期値0で表示なし)
+                    )
+        fontsiza_sv.pack()
+        
+        lnumcomment = ttk.Label(option_window, text=str(self.scale_var),wraplength=ww)
+        lnumcomment.pack()
 
-        lfontsize = ttk.Label(option_window, text="Fontsize",
-                            wraplength=ww)
-        lfontsize.pack()
-        txt2 = tkinter.Entry(option_window, width=20)
-        txt2.insert(tkinter.END, fontsize)
-        txt2.pack()
+        # lfontsize = ttk.Label(option_window, text="Fontsize",
+                            # wraplength=ww)
+        # lfontsize.pack()
+        # txt2 = tkinter.Entry(option_window, width=20)
+        # txt2.insert(tkinter.END, self.fontsize)
+        # txt2.pack()
 
-        lfontcolour = ttk.Label(option_window, text="Font colour",
-                                wraplength=ww)
-        lfontcolour.pack()
-        txt3 = tkinter.Entry(option_window, width=20)
-        txt3.insert(tkinter.END, fontcolour)
-        txt3.pack()
+        # lfontcolour = ttk.Label(option_window, text="Font colour",
+        #                         wraplength=ww)
+        # lfontcolour.pack()
+        # txt3 = tkinter.Entry(option_window, width=20)
+        # txt3.insert(tkinter.END, self.fontcolour)
+        # txt3.pack()
 
-        label = tkinter.Label(
-        self.root,
-        text="0",
-        font=("", 80),
-            width=10,
-            # textvariable=self.scale_var.get() # ウィジェット変数を設定
-        )
-        label.pack(padx=10, pady=10)
+        # label = tkinter.Label(
+        # self.root,
+        # text="0",
+        # font=("", 80),
+        #     width=10,
+        #     # textvariable=self.scale_var.get() # ウィジェット変数を設定
+        # )
+        # label.pack(padx=10, pady=10)
 
-        lalpha = ttk.Label(option_window, text="y-axis correction(If positive,display above)",
-                        wraplength=ww)
-        lalpha.pack()
-        txt4 = tkinter.Entry(option_window, width=20)
-        txt4.insert(tkinter.END, alpha)
-        txt4.pack()
+        # lalpha = ttk.Label(option_window, text="y-axis correction(If positive,display above)",
+        #                 wraplength=ww)
+        # lalpha.pack()
+        # txt4 = tkinter.Entry(option_window, width=20)
+        # txt4.insert(tkinter.END, self.alpha)
+        # txt4.pack()
 
-        bl1 = tkinter.BooleanVar(option_window)
-        bl1.set(True)
-        CheckBox1 = tkinter.Checkbutton(
-            option_window, text="Bold", variable=bl1)
-        CheckBox1.pack()
+        # bl1 = tkinter.BooleanVar(option_window)
+        # bl1.set(True)
+        # CheckBox1 = tkinter.Checkbutton(
+        #     option_window, text="Bold", variable=bl1)
+        # CheckBox1.pack()
         btn = ttk.Button(option_window, text="Start", command=self.start_btn)
         btn.pack(side="right")
         applybtn = ttk.Button(option_window, text="Stop", command=self.stop_btn)
         applybtn.pack(side="right")
+        # self.slider_scroll()
         self.root.mainloop()
-    def slider_scroll(self, event=None):
+    
+    def slider_scroll(self, event=0):
         '''スライダーを移動したとき'''
+        self.mic_label.place_forget()
+        self.scale_var.set(event)
+        # self.fontsize = int(self.scale_var.get())
+        # self.mic_label = ttk.Label(  self.root, 
+        #                         textvariable=self.mic_progres,
+        #                         wraplength=100, 
+        #                         font=("メイリオ", self.fontsize, "bold"),
+        #                         foreground=self.fontcolour,
+        #                         background="red" ) 
+        self.mixer_label.place(x=0, y=(100-self.num_comment*20-self.alpha)+int(self.scale_var.get()))  # -αは下のタスクバーの分
+        self.mic_label.place(x=0, y=(500-self.num_comment*20-self.alpha)+int(self.scale_var.get()))  # -αは下のタスクバーの分
         # label.config(text=str(self.scale_var.get()))
-        # print(str(self.scale_var.get()))
-
+        
+        # self.mixer_label.place_forget()
+        # self.mixer_scale_potision.set(event)
+        # # self.fontsize = int(self.scale_var.get())
+        # self.mixer_label = ttk.Label(  self.root, 
+        #                     textvariable=self.mic_progres,
+        #                     wraplength=100, 
+        #                     font=("メイリオ", self.fontsize, "bold"),
+        #                     foreground=self.fontcolour,
+        #                     background="red" ) 
+        # self.mixer_label.place(x=0, y=self.mixer_y_place+int(self.scale_var.get()))  # -αは下のタスクバーの分
+        # label.config(text=str(self.scale_var.get()))
+    
     def update_stt_result(self):
         tmp = ttsMIC.get_progress_result()
         self.mic_progres.set(tmp)
         tmp = ttsMIXER.get_progress_result()
         self.mixer_progres.set(tmp)
         self.root.after(10, self.update_stt_result)
+
     def stop_btn(self):
         ttsMIXER.set_stt_status(False)
         ttsMIC.set_stt_status(False)
-        # global start_flag
-        # start_flag = False
+
     def start_btn(self):
         ttsMIC.init_object()
         ttsMIXER.init_object()
@@ -205,4 +237,5 @@ def addwriteCsvTwoContents(AUDIO_FILE_NAME, ResultList:list, num, saveFileName, 
 
 if __name__ == '__main__':
     # draw_window()
-    display_result()
+    aa = display_result()
+    
